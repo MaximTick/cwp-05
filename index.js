a1d36e6abdcb56c2559cc086b278845e300b9d29
Maxim <maxim.tickhonovich@gmail.com>
2018-10-10 14:36:15 +0300

    Second task

--- index.js
+++ index.js
@@ -11,4 +11,16 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostnmae, () => {
     console.log(`Server running at http://${hostnmae}:${port}/`)
-})
+});
+
+function parseBodyJson(req, cb) {
+    req.on('data', function(chunk) {
+        body.push(chunk);
+    }).on('end', function() {
+        body = Buffer.concat(body).toString();
+
+        let params = JSON.parse(body);
+
+        cb(null, params);
+    });
+}
