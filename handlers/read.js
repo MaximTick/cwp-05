fb4658557bcd6193c2a8576115aecb705cc585a4
Maxim <maxim.tickhonovich@gmail.com>
2018-10-14 13:19:35 +0300

    read

---
+++ handlers/read.js
@@ -0,0 +1,24 @@
+const log = require("../log.js");
+const file = require("fs").createWriteStream("log.txt");
+const valid = require("./valid.js");
+let articles = require("../articles.json");
+
+const ErrorObject = {
+    code: 400,
+    message: 'Request invalid'
+};
+
+module.exports.read = function (req, res, payload, cb) {
+    if (valid.valid(req.url, payload) == true) {
+        let id = payload.id;
+        let article = {};
+        articles.forEach(element => {
+            if (element.id == id) {
+                article = element;
+            }
+        });
+        cb(null, article);
+    } else {
+        cb(ErrorObject);
+    }
+};
---
+++ handlers/readAll.js
@@ -0,0 +1,6 @@
+const log = require("../log.js");
+let articles = require("../articles.json");
+
+module.exports.readAll = function (req, res, payload, cb) {
+    log.log(req.url, JSON.stringify(payload));
+};
